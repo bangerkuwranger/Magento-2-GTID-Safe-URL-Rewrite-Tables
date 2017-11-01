39c0cd764009a07763c1d1d2ff905426a9cfd0d0
bingollu <bbingollu@sundialbrands.com>
2017-11-01 11:49:27 -0400

    Update InstallSchema.php

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -6,9 +6,7 @@ use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
-/**
- * @codeCoverageIgnore
- */
+
 class InstallSchema implements InstallSchemaInterface
 {
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
