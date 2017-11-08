41b4fe1b4d96af2c2af7f27d29085424cf2cba7a
Chad A. Carino <ccarino@sundialbrands.com>
2017-11-08 10:21:39 -0500

    add missing registration file

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2017 Chad A. Carino. All rights reserved.
+ * See LICENSE file for license details.
+ *
+ * @package    Bangerkuwranger/GtidSafeUrlRewriteTables
+ * @author     Chad A. Carino <artist@chadacarino.com>
+ * @author     Burak Bingollu <burak.bingollu@gmail.com>
+ * @copyright  2017 Chad A. Carino
+ * @license    https://opensource.org/licenses/MIT  MIT License
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Bangerkuwranger_GtidSafeUrlRewriteTables',
+    __DIR__
+);
